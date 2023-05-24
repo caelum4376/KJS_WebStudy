b9594aa2166a061414877fe5ae20ae26444b4943
caelum4376 <129152864+caelum4376@users.noreply.github.com>
2023-05-24 18:16:05 +0900

    freeboard 초기작성

---
+++ HTMLProject01/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,14 @@
+package com.sist.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		try {
+			new Thread().suspend();
+			
+		} catch (Exception e) {
+			
+		}
+	}
+
+}
